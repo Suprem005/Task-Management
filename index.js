a944a555224c8bd66722fa35ad896d42815f9ecf
Suprem005 <suprem.sthao7@gmail.com>
2026-02-10 17:28:04 +0545

    feat: DB connection established

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import connectDB from "./modules/services/db.connect.js";
+const app = express();
+
+// to make app understand json
+app.use(express.json());
+
+// TODO: enable CORS
+
+// connect DB
+connectDB();
+//  register routes
+
+// TODO: handle global errors
+
+// network port and server
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT ${PORT}`);
+});
